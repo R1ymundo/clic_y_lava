209190996bd6b6ec1660d70fd4bcab49e8cf6d71
karlaors <karlaodethrs@gmail.com>
2025-04-24 17:30:25 -0600

    Se agregan los estilos del footer

--- js/scriptFooter.js
+++ js/scriptFooter.js
@@ -1,26 +1,31 @@
 const agregarFooter = document.getElementById("agregarFooter");
 
-agregarFooter.insertAdjacentHTML("afterbegin",
-    `<div class="row" style="background-color: #FEB36D;">
-        <div class="col-sm-6 mb-3 mb-sm-0">
-            <div class="container">
-                <img src="../assets/icons/logo_clic_lava.png"  style="width:5rem; display: block; margin: 0 auto;" >
-                <p style="text-align: center;"><img src="../assets/icons/correo.webp"style="width:2rem; display: block; margin: 0 auto;">cliclava@gmail.com</p>
-                <a href="https://github.com/R1ymundo/clic_y_lava"> <img src="../assets/icons/github.webp" style="width:2rem; display: block; margin: 0 auto;"></a>
-            </div>
+agregarFooter.insertAdjacentHTML("afterbegin", `
+  <div class="row py-4" style="background-color: #FFB36D; text-align: center; align-items: center;">
+    <div class="col-sm-6 mb-4 d-flex flex-column justify-content-center align-items-center">
+      <div class="container">
+        <img src="../assets/icons/logo_clic_lava.png" style="width:5rem; display:block; margin:0 auto;">
+        <div style="margin-top: 1rem;">
+          <a href="mailto:cliclava@gmail.com">
+            <img src="../assets/icons/correo.webp" style="width:2rem; margin: 0 .5rem;">
+          </a>
+          <a href="https://github.com/R1ymundo/clic_y_lava/tree/develop">
+            <img src="../assets/icons/github.webp" style="width:2rem; margin: 0 .5rem;">
+          </a>
         </div>
+        <p style="margin-top: 0.5rem;">cliclava@gmail.com</p>
+      </div>
+    </div>
 
-        <div class="col-sm-6 mb-3 mb-sm-0"> 
-            <div class="container">
-               <p>Proyecto integrador desarrollado en Generation México</p>
-              <a href ="https://mexico.generation.org/"><img src="../assets/icons/generation.webp" style="width:3rem;"></a>
-            </div>
-        </div>
-                 
-
-                
-    </div>`
-
-);
+    <div class="col-sm-6 mb-4 d-flex flex-column justify-content-center align-items-center">
+      <div class="container text-center">
+        <p style="margin-bottom: 0.5rem;">Proyecto integrador desarrollado en Generation México</p>
+        <a href="https://mexico.generation.org/">
+          <img src="../assets/icons/generation.webp" style="width:6rem;">
+        </a>
+      </div>
+    </div>
+  </div>
+`);
 
 
