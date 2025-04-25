3e57c30084ef64cc28cd1deaa4883db204af0758
emj <eduard.maju@gmail.com>
2025-04-24 18:44:47 -0600

    Se crea un script que agrega el nombre del proyecto

---
+++ js/scriptEncabezado.js
@@ -0,0 +1,9 @@
+const encabezado = document.getElementById("encabezado");
+
+encabezado.insertAdjacentHTML("afterbegin",
+    `<div class="row py-4 justify-content-center align-items-center" style="background-color: #FFB36D; color: #F5FDFF; font-family: 'Grandstander', cursive; text-align: center;">
+        <div class="col-sm-6 text-center">
+            <h1 style="font-size:30px">Proyecto Clic & Lava</h1>
+        </div>
+    </div>`
+);
