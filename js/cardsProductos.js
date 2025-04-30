eaa345abdc98112712a457ce56281a1f8a28c71b
Ricardo MR <ricardomr.23@hotmail.com>
2025-04-29 19:17:47 -0600

    Se agrega fetch y reenderizado de cards de productos

--- js/cardsProductos.js
+++ js/cardsProductos.js
@@ -1,6 +1,7 @@
 const cardsProduct = document.getElementById("cardsProduct");
+const alertData = document.getElementById("alertData");
 
-
+// Array de objetos (productos a reenderizzar) ->
 let productos = [
   {
     id: 1,
@@ -124,18 +125,57 @@ let productos = [
   },
 ];
 
-
+// Se almacenan los productos en localStorage (comentar o descomentar para pruebas de error) ->
 localStorage.setItem("productos", JSON.stringify(productos));
 
-cardsProduct.insertAdjacentHTML("beforeend", 
-      `<div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">titulo</h5>
-            <h5 class="card-title">marca</h5>
-            <h5 class="card-title">precio</h5>
-            <a href="#" class="btn btn-primary">Ver más detalles..</a>
-          </div>
-      </div>`);
+// Promesa para productos ->
+const getData = async () => {
+  return new Promise((resolve, reject) => {
+    const getProductos = JSON.parse(localStorage.getItem("productos"));
+    if (getProductos == null) {
+      reject(new Error("¡Ocurrio un error, no se puede acceder a los datos!"));
+    } else {
+      resolve(getProductos);
+    }
+  });
+};
 
-
+// Funcion para hacer fetch de los datos ocapturar errores con funcion asincrona ->
+const fetchingProducts = async () => {
+  try {
+    const arrayProductos = await getData();
+    const mostrarCard = arrayProductos
+      .map(producto => {
+        return `
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                  <div class="card h-100">
+                    <img src="${producto.imagen}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-title">${producto.modelo}</p>
+                      <p class="card-title">Marca ${producto.marca}</p>
+                      <p class="card-title">Renta $<strong>${producto.precioRenta}</strong></p>
+                      <a href="" class="btn btn-primary">Ver más detalles..</a>
+                    </div>
+                  </div> 
+                </div>
+              `;
+      })
+      .join("");
+    cardsProduct.innerHTML = mostrarCard;
+  } catch (error) {
+    alertData.insertAdjacentHTML(
+      "beforeend",
+      `
+            <div class="alert alert-danger d-flex" role="alert">
+                <svg xmlns="http://www.w3.org/2000/svg" class="bi flex-shrink-0 me-2" width="50px" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                  <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </svg>
+                <div>
+                    <strong>${error.message}</strong>
+                </div>
+            </div>
+        `
+    );
+  }
+};
+fetchingProducts();
