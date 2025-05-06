d2ba5d667a9a5ea9dd4ded8b1b367441993404da
sandy <sandra.de.la.sancha.aguilar.isc.2000@gmail.com>
2025-05-06 13:17:07 -0600

    Se crea el archivo js para comenzar con el localStorage

---
+++ js/formularioRegistro.js
@@ -0,0 +1,76 @@
+let usuarios = [
+    {
+        id: 1,
+        nombre: "Sandy",
+        apellido: "De la Sancha Aguilar",
+        telefono: 5610045844,
+        email: "sandy123@gmail.com",
+        contraseña: 12345
+    }, 
+    
+    {
+        id: 2,
+        nombre: "Ricardo",
+        apellido: "Miguel Raya",
+        telefono: 5610331257,
+        email: "ricardor143@gmail.com",
+        contraseña: 12345
+    },
+
+    {
+        id: 3,
+        nombre: "Karla Odeth",
+        apellido: "Ruiz Sánchez",
+        telefono: 5510254689,
+        email: "karlita156@gmail.com",
+        contraseña: 12345
+
+    },
+
+    {
+        id: 4,
+        nombre: "Ana Gabriela",
+        apellido: "García García",
+        telefono: 5556152367,
+        email: "anaharcia156@gmail.com",
+        contraseña: 12345
+    },
+
+    {
+        id: 5,
+        nombre: "Jesus",
+        apellido: "Galicia Rueda",
+        telefono: 598547763,
+        email: "galicia156@gmail.com",
+        contraseña: 12345
+    },
+
+    {
+        id: 6,
+        nombre: "Eduardo",
+        apellido: "Martínez Juárez",
+        telefono: 5556152345,
+        email: "edug154@gmail.com",
+        contraseña: 12345
+    },
+
+    {
+        id: 7,
+        nombre: "Raymundo",
+        apellido: "Mendoza Ramirez",
+        telefono: 5878331456,
+        email: "raymundom@gmail.com",
+        contraseña: 12345
+    },
+
+    {
+        id: 8,
+        nombre: "Anthony",
+        apellido: "Velázquez Reyes",
+        telefono: 5589412368,
+        email: "anthonyvqz@gmail.com",
+        contraseña: 12345
+    }
+]
+
+
