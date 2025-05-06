f536d38844b0361c352c3e72d3c7f44001cc64a3
Raymundo Mendoza Ramirez <raymundomendozaramirez@gmail.com>
2025-05-05 21:26:40 -0600

    Se agrega producto mediante el id

--- js/detalleProducto.js
+++ js/detalleProducto.js
@@ -1,15 +1,40 @@
 const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
 const id = searchParams.get("id")
-console.log(searchParams.get("id"));
-console.log(searchParams.get("categoria"));
 
-let productos = localStorage.getItem("productos");
-productos = JSON.parse(productos)
+const imgLavadora = document.getElementById("imgLavadora");
+const tituloLavadora = document.getElementById("tituloLavadora");
+const sku = document.getElementById("sku");
+const precio = document.getElementById("precio");
+const caracteristicas1 = document.getElementById("caracteristicas1");
+const caracteristicas2 = document.getElementById("caracteristicas2");
+const descripcion = document.getElementById("descripcion");
 
-console.log(productos)
+// Obtener el objeto del localStorage
+let productData = localStorage.getItem("productData");
+productData = JSON.parse(productData);
 
-let producto = productos.find( produc => produc.id === Number(id));
+//Obtener los productos
+let productos = productData.productos;
 
-console,log(producto);
+// Buscar el producto por ID
+const producto = productos.find(p => p.id === Number(id));
+
+if (producto) {
+    
+    imgLavadora.insertAdjacentHTML("afterbegin", 
+        `<img src="${producto.imagenes.imagenPricipal}" alt="Lavadora" class="img-fluid"></img>`);
+    
+    tituloLavadora.innerText = producto.modelo;
+    sku.innerText = `SKU: ${producto.stock}`;
+    precio.innerText = `$${producto.precios.precioDia} por día`
+
+    caracteristicas1.innerText = producto.caracteristicas.caracteristica1;
+    caracteristicas2.innerText = producto.caracteristicas.caracteristica2;
+
+    descripcion.innerText = producto.descripcion;
+
+
+}
+
 
