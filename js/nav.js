fc3a35a83ffecd7638c0015a21575973d71ada3a
Raymundo Mendoza Ramirez <raymundomendozaramirez@gmail.com>
2025-05-12 13:41:11 -0600

    Se agrega accion de cerrar sesion

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,10 @@
 const navegacion = document.getElementById("navegacion");
 const sesion = localStorage.getItem("sesionIniciada");
-const inicioSesion = document.getElementById ("inicioSesion");
-const cerrarSesion = document.getElementById ("cerrarSesion");
 
-if (sesion === "true"){
-    inicioSesion.style.display = "none";
-    cerrarSesion.style.display = "inline";
-
-}
 navegacion.insertAdjacentHTML(
-  "afterbegin",
-  ` 
-     <div class="container-fluid">
+    "afterbegin",
+    ` 
+    <div class="container-fluid">
             <div class="col-5">
                 <a class="navbar-brand" href="../index.html">Clic y lava</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -33,15 +26,16 @@ navegacion.insertAdjacentHTML(
                     </li>
             </div>
             </ul>
-            <a class="nav-link" href="../paginas/inicioSesion.html" style="margin-right: 1rem; display:none">Iniciar sesión
-                <img src="../assets/icons/login.png" alt="login" width="24" height="24">
-            </a>
-            
+
             <a class="nav-link" href="../paginas/compras.html" style="margin-right: 1rem;">Mis Compras
                 <img src="../assets/icons/carrito.webp" alt="Carrito" width="24" height="24">
             </a>
 
-            <a class="nav-link" href="../paginas/inicioSesion.html" style="margin-right: 1rem; display:inline">Cerrar sesión
+            <a id="inicioSesion" class="nav-link" href="../paginas/inicioSesion.html" style="margin-right: 1rem; display:inline">Iniciar sesión
+                <img src="../assets/icons/login.png" alt="login" width="24" height="24">
+            </a>
+
+            <a id="cerrarSesion" class="nav-link" style="margin-right: 1rem; display:none">Cerrar sesión
                 <img src="../assets/icons/out.png" alt="salir" width="24" height="24">
             </a>
 
@@ -49,3 +43,36 @@ navegacion.insertAdjacentHTML(
         </div>  
     `
 );
+
+
+const inicioSesion = document.getElementById("inicioSesion");
+const cerrarSesion = document.getElementById("cerrarSesion");
+
+if (sesion === "true") {
+    inicioSesion.style.display = "none";
+    cerrarSesion.style.display = "inline";
+
+} 
+
+cerrarSesion.addEventListener("click", function (event) {
+    event.preventDefault();
+    
+    //Cambiar el estado de los a
+    inicioSesion.style.display = "inline";
+    cerrarSesion.style.display = "none";
+
+    // Borrar los datos de sesión
+    localStorage.removeItem("sesionIniciada");
+    localStorage.removeItem("usuarioActivo");
+    
+
+    // Opcional: Mostrar alerta
+    Swal.fire({
+        title: "Sesión cerrada",
+        text: "Has cerrado sesión correctamente",
+        icon: "success",
+    }).then(() => {
+        // Redirigir después de cerrar sesión
+        window.location.href = "../paginas/acerca.html"; // o donde desees
+    });
+});
